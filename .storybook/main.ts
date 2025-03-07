4561b0061400140dd27e517d2f5037e137b50728
myjeong19 <myjeong19@naver.com>
2025-03-07 17:21:21 +0900

    fix. Storybook migration

---
+++ .storybook/main.ts
@@ -0,0 +1,36 @@
+import type { StorybookConfig } from '@storybook/experimental-nextjs-vite';
+
+import { join, dirname } from 'path';
+
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, 'package.json')));
+}
+const config: StorybookConfig = {
+  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+
+  addons: [
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@storybook/addon-onboarding'),
+    getAbsolutePath('@chromatic-com/storybook'),
+    getAbsolutePath('@storybook/experimental-addon-test'),
+    getAbsolutePath('@storybook/addon-mdx-gfm'),
+    '@storybook/addon-mdx-gfm',
+  ],
+
+  framework: '@storybook/nextjs',
+
+  staticDirs: ['../public'],
+
+  docs: {
+    autodocs: true,
+  },
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
+  },
+};
+export default config;
